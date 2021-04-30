99f7d5c04dc4ebe54c7cf18db4eb6628631d8fa6
yoshi <captain3672@gmail.com>
2021-04-30 08:49:44 +0000

    add column

--- db/migrate/20210428025217_add_title_to_status.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToStatus < ActiveRecord::Migration[5.2]
-  def change
-    add_column :statuses, :title, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_062612) do
+ActiveRecord::Schema.define(version: 2021_04_30_083106) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
