21cd077a5dc69dddbf0b35e3eeed8f3face6f415
yoshi <captain3672@gmail.com>
2021-05-17 07:38:09 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+    root to: 'toppages#index'
     
-    resources :tasks
+    get 'signup', to: 'users#new'
+    resources :users , only: [:index, :show, :create]
 end
